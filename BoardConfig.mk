56d12f791ba01a90feb5bc16113dd52e62393040
khanfrd <khanfrd94@gmail.com>
2018-03-27 20:44:12 +0530

    mido: Initial common tree
     * https://github.com/TheScarastic/android_device_xiaomi_mido/commit/76f0d0114e56c4bf080517fbf316b9a0c26a8bd0

---
+++ BoardConfig.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from common msm8953-common
+-include device/xiaomi/msm8953-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/xiaomi/mido
+
+# Filesystem
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+
+# Kernel
+TARGET_KERNEL_CONFIG := mido_defconfig
+
+# Partitions
+BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 25765043200 # 25765059584 - 16384
+
+# Inherit from the proprietary version
+-include vendor/xiaomi/mido/BoardConfigVendor.mk
